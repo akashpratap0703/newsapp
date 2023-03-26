f7b3e2019cf11b20d4d528eeb8601f0ccd3446b0
akashpratap0703 <akashpratap0703@gmail.com>
2023-03-26 20:47:04 +0530

    [26/3/2023]

--- src/components/News.js
+++ src/components/News.js
@@ -23,32 +23,24 @@ export class News extends Component {
             totalSize:0,
         }
     }
-  async componentDidMount(){
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=71567857be1f4570afefafb4b429a4d1&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews(){
+    const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=71567857be1f4570afefafb4b429a4d1&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data =await fetch(url);
     let parsedData=await data.json();
     this.setState({articles:parsedData.articles,totalSize:parsedData.totalResults})
   }
+  async componentDidMount(){
+    this.updateNews();
+  }
   handleNextClick = async ()=>{
-    if(this.state.page + 1 > Math.ceil(this.state.totalSize/this.props.pageSize)){}else{
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=71567857be1f4570afefafb4b429a4d1&page=${this.state.page +1}&pageSize=${this.props.pageSize}`;
-    let data =await fetch(url);
-    let parsedData=await data.json();
-    this.setState({
-        articles:parsedData.articles,
-        page:this.state.page + 1,
-    })
-}
+  this.setState({ page:this.state.page + 
+    1});
+  this.updateNews();
   }
 
   handlePrevClick = async ()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=71567857be1f4570afefafb4b429a4d1&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-    let data =await fetch(url);
-    let parsedData=await data.json();
-    this.setState({
-        articles:parsedData.articles,
-        page:this.state.page - 1,
-    })
+    this.setState({ page:this.state.page - 1});
+    this.updateNews();
   }
   render() {
     return (
@@ -58,7 +50,7 @@ export class News extends Component {
         <div className="row">
             {this.state.articles.map((element)=>
             {return <div className="col-md-4"  key={element.url} >
-                < NewsItem title={element.title!=null?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                < NewsItem title={element.title!=null?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
             </div>})}  
         </div>
         <div className="contianer d-flex justify-content-between">
