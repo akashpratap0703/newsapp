23af2d0b59d204194d68a8538fab77ec693c6515
akashpratap0703 <akashpratap0703@gmail.com>
2023-05-14 20:24:32 +0530

    Add about to portfolio

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,16 +5,16 @@ import '../css/navbar.css';
 export default function NavBar() {
   return (
     <div>
-    <div className='container'>
+    <div className="container">
     <nav>
-    <img src="./logo192.png" className='logo'/>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Service</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
+        <img src="./logo192.png" className="logo"/>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
     </nav>
     </div>
     </div>
